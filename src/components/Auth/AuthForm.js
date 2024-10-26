ece3a500c9255f9d948e035b091fcba2c6a83431
HarshitNegi-A <harshitnegi226@gmail.com>
2024-10-26 10:24:59 +0530

    Implementing Login [DIY]

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -7,7 +7,6 @@ const AuthForm = () => {
   const emailInputRef=useRef();
   const passwordInputRef=useRef();
   const [isLoading,setIsLoading]=useState(false)
-
   const [isLogin, setIsLogin] = useState(true);
 
   const switchAuthModeHandler = () => {
@@ -21,35 +20,39 @@ const AuthForm = () => {
     const enteredPassword=passwordInputRef.current.value
 
     setIsLoading(true)
+    let url;
     if(isLogin){
-
+      url='https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAc1hoElOXf55Mb8l-jZ0GVGHn5SWBcndc'
     }else{
-      fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAc1hoElOXf55Mb8l-jZ0GVGHn5SWBcndc',{
-        method:'POST',
-        body: JSON.stringify({
-          email:enteredEmail,
-          password:enteredPassword,
-          returnSecureToken: true,
-        }),
-        headers :{
-          'Content-Type' : 'application/json'
-        }
-      }).then(res =>{
-        setIsLoading(false)
-        if(res.ok){
-
-        }
-        else{
-          return res.json().then(data=> {
-            let errorMessage='Authetication Failed'
-            if(data && data.error && data.error.message){
-              errorMessage=data.error.message;
-            }
-             alert(errorMessage)
-          });
-        }
-      })
+      url='https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAc1hoElOXf55Mb8l-jZ0GVGHn5SWBcndc'
     }
+    fetch(url,{
+      method:'POST',
+      body: JSON.stringify({
+        email:enteredEmail,
+        password:enteredPassword,
+        returnSecureToken: true,
+      }),
+      headers :{
+        'Content-Type' : 'application/json'
+      }
+    }).then(res =>{
+      setIsLoading(false)
+      if(res.ok){
+        return res.json();
+      }
+      else{
+        return res.json().then(data=> {
+          let errorMessage='Authetication Failed'
+           throw new Error(errorMessage);
+        });
+      }
+    }).then(data =>{
+      console.log(data)
+    })
+    .catch((err)=>{
+      alert(err.message)
+    })
 
 
   }
